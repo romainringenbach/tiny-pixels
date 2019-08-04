cc41c71d6b04292fbf635b85ce00c67a650f9533
nealith <nealith@neaworld.fr>
2019-08-05 00:11:01 +0200

    begin change in program struct

--- src/node.ts
+++ src/node.ts
@@ -2,18 +2,16 @@ import {Transform} from "./transform";
 
 export class Node {
 
-  public transform : Transform;
-  public readonly childs : any
+  private childs : { [id: string] : Node; };
   public parent : Node | null;
 
-  public constructor(name : string, parent : Node | null){
-
+  protected constructor(){
     this.transform = new Transform();
     this.childs = {};
-    this.parent = parent;
+    this.parent = null;
   }
 
-  public addChild(name:string,child:Node){
+  protected addChild(name:string,child:Node){
     if (this.childs[name] === undefined) {
         this.childs[name] = child;
     } else {
@@ -21,12 +19,52 @@ export class Node {
     }
   }
 
+  protected getNode(path:string){ // get node with something like name1.name2.name3... etc
+
+  }
+
+  private _process(delta : number){
+    for (var name in this.childs) {
+      this.childs[name]._process(delta)
+    }
+    this.process(delta);
+  }
+
   public process(delta : number){
 
+  }
+
+  private _draw(delta : number){
+    this.matrices_stack.apply(this.transform);
+    for (var name in this.childs) {
+      this.childs[name]._draw(delta)
+    }
+    this.draw(delta);
+    this.matrices_stack.pop();
   }
 
   public draw(delta : number){
 
   }
 
+}
+
+export class Node2D extends Node {
+  public transform : Transform;
+  public constructor (){
+    super();
+  }
+  public addChild(name:string,child:Node2D){
+    super.addChild(name,child);
+  }
+}
+
+export class Scene extends Node {
+  public constructor (){
+    super();
+    this.parent = null;
+  }
+  public addChild(name:string,child:Node){
+    super.addChild(name,child);
+  }
 }
--- src/tiny_pixels.ts
+++ src/tiny_pixels.ts
@@ -1,7 +1,60 @@
-export * from "./engine";
-export * from "./game";
-export * from "./maths";
-export * from "./node";
-export * from "./program";
-export * from "./transform";
-export * from "./error";
+
+import {MatricesStack} from "./matrices_stack";
+import {Node} from "./node";
+import {Program} from "./program";
+import {Mat4} from "./maths";
+import {Error} from "./error";
+
+export class TinyPixels extends Node {
+
+    private canvas : HTMLCanvasElement
+    private programs : { [id: string] : Program; };
+    private matrices_stack : MatricesStack;
+    private gl_ctx : any;
+    private current_scene : Scene | null;
+    private last_call : number
+
+    public constructor(canvas : HTMLCanvasElement){
+      this.programs = {};
+      this.matrices_stack = new MatricesStack();
+      this.canvas = canvas;
+      this.gl_ctx = this.canvas.getContext("webgl");
+      this.current_scene = null;
+      this.last_call = -1;
+    }
+
+    public addProgram(name: string,vertex_source: string,fragment_source: string){
+      if (this.programs[name] === undefined) {
+        this.programs[name] = new Program(vertex_source,fragment_source);
+        this.programs[name]._create(this.gl_ctx);
+      } else {
+        throw new DictionnaryError("Adding program of name: "+name+" failed",DictionnaryErrorType.AlreadyPresent)
+      }
+    }
+
+    public launchScene(scene : Node){
+      this.current_scene = scene;
+    }
+
+    private oneIter(){
+      if (this.current_scene != null) {
+          let delta = 0;
+          if (this.last_call == -1) {
+              this.last_call == Date.now();
+          } else {
+              let now = Date.now();
+              delta = now - this.last_call;
+              this.last_call == now;
+          }
+          this._process(delta)
+          this.matrices_stack.push(Mat4.orthographic(0, this.gl_ctx.canvas.width, this.gl_ctx.canvas.height, 0, -1, 1));
+          this._draw(delta);
+          this.matrices_stack.pop()
+      }
+    }
+
+    public run(){
+      let engine = this;
+      window.requestAnimationFrame(engine.oneIter);
+    }
+}
