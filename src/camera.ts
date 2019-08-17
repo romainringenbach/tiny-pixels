c44f9772471f7ce91a7c5286c97a39832e282fb9
nealith <nealith@neaworld.fr>
2019-08-17 12:08:34 +0200

    add follow node feature on camera

--- src/camera.ts
+++ src/camera.ts
@@ -1,16 +1,20 @@
 import {Mat4} from "./maths";
 import {Transform} from "./transform";
-import {Engine} from "./engine"
+import {Engine} from "./engine";
+import {Node} from "./node";
 
 export class Camera {
     public projection_matrix : number[];
     public view_matrix : number[];
     public transform : Transform;
 
+    private nodeToFollow : number;
+
     public constructor(){
       this.transform = new Transform();
       this.projection_matrix = [];
       this.view_matrix = [];
+      this.nodeToFollow = 0;
     }
 
     public ready(gl:any,engine:Engine){
@@ -18,8 +22,22 @@ export class Camera {
     }
 
     public process(gl:any,engine:Engine){
+
+      if (this.nodeToFollow != 0) {
+        this.transform = Node.getNodeById(this.nodeToFollow).getGlobalTransform();
+      }
+
       this.view_matrix = Mat4.translation(-this.transform.translation_x,-this.transform.translation_y,-this.transform.z_index)
       Mat4.mul(this.view_matrix,Mat4.rotation(-this.transform.rotation));
       Mat4.mul(this.view_matrix,Mat4.scale(-this.transform.scale_x,-this.transform.scale_y));
+    }
+
+    public followNode(node:Node) {
+      this.nodeToFollow = node.id;
+    }
+
+
+    public stopFollowNode() {
+      this.nodeToFollow = 0;
     }
 }
